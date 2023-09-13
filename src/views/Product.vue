578045023b45e3a86d19e9eef7b54b9a014d0c40
mistarisu <140592647+mistarisu@users.noreply.github.com>
2023-09-13 15:15:00 +0700

    Update warna

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -23,7 +23,7 @@ function addToCart(product) {
 <template>
   <!-- Breadcrumbs -->
   <nav class="flex items-center gap-3 mb-4">
-    <router-link to="/" class="text-gray-300 font-semibold text-sm flex items-center hover:text-white">
+    <router-link to="/" class="text-black-300 font-semibold text-sm flex items-center hover:text-black">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
            class="w-4 h-4 mr-1.5">
         <path stroke-linecap="round" stroke-linejoin="round"
@@ -31,8 +31,8 @@ function addToCart(product) {
       </svg>
       Home
     </router-link>
-    <p class="text-gray-200">/</p>
-    <p class="text-gray-200 text-sm">
+    <p class="text-black-200">/</p>
+    <p class="text-black-200 text-sm">
       {{ product.name }}
     </p>
   </nav>
@@ -41,20 +41,20 @@ function addToCart(product) {
   <!-- Product -->
   <div class="">
     <img :src="product.image" class="w-full h-60 object-contain rounded-lg">
-    <h4 class="mt-4 text-gray-100 text-2xl font-semibold">
+    <h4 class="mt-4 text-black-100 text-2xl font-semibold">
       {{ product.name }}
     </h4>
-    <p class="text-gray-300 mt-2 text-sm">
+    <p class="text-black-300 mt-2 text-sm">
       {{ product.description }}
     </p>
 
     <!-- Add to cart -->
     <div class="flex items-center gap-16 mt-6">
-      <h4 class="font-semibold text-gray-100 text-2xl">Rp{{ parseFloat(product.price).toFixed(2) }}</h4>
+      <h4 class="font-semibold text-black-100 text-2xl">Rp{{ parseFloat(product.price).toFixed(2) }}</h4>
       <div v-if="isInCart"
-           class="flex gap-3 items-center justify-between px-2.5 font-semibold w-full text-center bg-cyan-500 text-white rounded-full py-1">
+           class="flex gap-3 items-center justify-between px-2.5 font-semibold w-full text-center bg-pink-500 text-white rounded-full py-1">
         <button @click="store.decreaseCart(product.id)"
-                class="flex-shrink-0 w-5 h-5 rounded-full border border-white text-white flex items-center justify-center hover:bg-cyan-700 transition active:scale-90">
+                class="flex-shrink-0 w-5 h-5 rounded-full border border-white text-black flex items-center justify-center hover:bg-pink-700 transition active:scale-90">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
                class="w-4 h-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15"/>
@@ -62,7 +62,7 @@ function addToCart(product) {
         </button>
         {{ isInCart }} in Cart
         <button @click="store.increaseCart(product.id)"
-                class="flex-shrink-0 w-5 h-5 rounded-full border border-white text-white flex items-center justify-center hover:bg-cyan-700 transition active:scale-90">
+                class="flex-shrink-0 w-5 h-5 rounded-full border border-white text-white flex items-center justify-center hover:bg-pink-700 transition active:scale-90">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
                class="w-4 h-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
@@ -70,7 +70,7 @@ function addToCart(product) {
         </button>
       </div>
       <button v-else @click="addToCart(product)"
-              class="flex items-center justify-center font-semibold w-full text-center bg-cyan-500 text-white rounded-full py-1 hover:bg-cyan-600 transition active:scale-90">
+              class="flex items-center justify-center font-semibold w-full text-center bg-pink-500 text-white rounded-full py-1 hover:bg-pink-600 transition active:scale-90">
         Add to Cart
       </button>
     </div>
